776f699d2aabc074075eec2c26dd64ad2ac8eaa4
Eni <enimirena@gmail.com>
2025-12-30 18:46:56 +0100

    Initial Auvier project setup

---
+++ src/main/java/com/auvier/AuvierApplication.java
@@ -0,0 +1,13 @@
+package com.auvier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuvierApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuvierApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/auvier/AuvierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.auvier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuvierApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
